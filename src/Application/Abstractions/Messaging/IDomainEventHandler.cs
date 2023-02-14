3969f9679948bd5d440c888470e4c10800656755
snmburuh <snmburuh@gmail.com>
2023-02-14 21:59:06 +0300

    Add Messaging Abstractions Add Validation Behaviors

---
+++ src/Application/Abstractions/Messaging/IDomainEventHandler.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Primitives;
+using MediatR;
+
+namespace Application.Abstractions.Messaging
+{
+    public interface IDomainEventHandler<TEvent> : INotificationHandler<TEvent>
+    where TEvent : IDomainEvent
+    {
+    }
+}
